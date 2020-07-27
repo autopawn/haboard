0dead5dad7f725017bb64191f5b6845cd9354a35
fanterox <ismaelcruz7H@gmail.com>
2020-07-27 17:14:39 -0400

    Agregado_SimpleWars

--- Piece.hs
+++ Piece.hs
@@ -8,7 +8,7 @@ import Data.Maybe (Maybe)
     Se hace el type synonym Piece que equivale a una tupla de cuatro elementos.
 -}
 
-type Piece = (Int,Int,Int,Char) -- (jugador, coordenada x, coordenada y, tipo)
+type Piece = (Int,Int,Int,Int) -- (jugador, coordenada x, coordenada y, Hp unidad)
 
 
 {-
@@ -46,6 +46,108 @@ moveName (xi,yi) (xf,yf) = let
     in (cxi:syi) ++ (cxf:syf)
 
 {-
+    alLado revisa si una pieza es vecina de otra del otro jugador
+    si encuentra encuentra una, le quita 1 punto de hp, sino simplemente 
+    no hace nada
+    Retorna una Piece
+-}
+
+alLado :: Piece -> Piece -> Piece
+alLado (pp,xx, yy,kk) (p,x,y,k)
+    | abs(x-xx) + abs(y-yy) == 1 && pp /= p = (p,x,y,k-1)
+    | otherwise                     = (p,x,y,k)
+
+{- 
+    punchNext recibe una Piece y le aplica la funcion alLado
+    a todas las unidades de la [Piece] que recibe
+-}
+
+punchNext :: Piece -> [Piece] -> [Piece]
+punchNext sq st = 
+    map (alLado sq) st
+
+{-
+    removerItem revisa si hay una unidad con hp = 0
+    si encuentra una, retorna la lista de unidades sin
+    esa pieza
+-}
+
+removerItem :: [Piece] -> [Piece]
+removerItem st = filter(\(p,x,y,k) -> 0 /= k) st
+
+{-
+    actualizarLista aplica las funciones removerItem y punchNext
+    recibe una Piece y una lista de Pieces
+    Retorna una lista de Pieces actualizada
+-}
+
+actualizarLista :: Piece -> [Piece] -> [Piece]
+actualizarLista sq li =
+    removerItem (punchNext sq li)
+
+{-
+    sumaHp y totalHp sirven para sumar el hp de todas las unidades
+    de un jugador
+    Retornan un Int
+-}
+
+sumaHp :: Piece -> Int
+sumaHp (p,x,y,k) = k
+
+totalHp :: [Piece] -> Int -> Int
+totalHp st pla = sum(map sumaHp (filter(\(p,x,y,k) -> p == pla)st))
+
+{-
+    Las funciones soldados 1 y 0 reciben un [Int]
+    Retornan una lista infinita de unidades de el jugador
+    0 y 1.
+-}
+
+soldados0 :: [Int] -> [Piece]
+soldados0 (x:y:rns) = (0,x `mod` 15, y `mod` 10, 3) : soldados0 rns
+
+soldados1 :: [Int] -> [Piece]
+soldados1 (x:y:rns) = (1,x `mod` 15, y `mod` 10, 3) : soldados1 rns
+
+{-
+    Posiciones 1 y 0 reciben un [Int] (lista de numeros infinitos)
+    es la misma lista que reciben soldados 0 y 1 para revisar 
+    las posiciones que tienen estas
+    Retorna una lista de posiciones (x,y) de las unidades de el jugador
+-}
+
+posiciones0 :: [Int] -> [(Int,Int)]
+posiciones0 (x:y:rns) = (x `mod` 15, y `mod` 10) : posiciones0 rns
+
+posiciones1 :: [Int] -> [(Int,Int)]
+posiciones1 (x:y:rns) = (x `mod` 15, y `mod` 10) : posiciones1 rns
+
+{-
+    Comparador toma dos listas de numeros infinitos, toma 4 posiciones de
+    las listas y compara las primeras 4 posiciones y revisa si hay se repite
+    alguna posicion entre las 2 listas.
+    Retorna un True si encuentra una, si no va a retornar un False
+-}
+
+comparador :: [Int] -> [Int] -> Bool
+comparador semilla0 semilla1
+    | True `elem` map(`elem` (take 4 (posiciones0 semilla0))) (take 4 (posiciones1 semilla1))  = True
+    | otherwise = False
+
+{-
+    unidades recibe una lista de Int infinitas, revisa si hay posiciones
+    repetidas entre las listas de posiciones, si encuentra que es True, hace 
+    un loop y bota 4 indices de las listas de Int infinitos.
+    Retorna una lista de 8 unidades de los jugadores, 4 del jugador 0
+    y 4 del jugador 1
+-}
+
+unidades :: [Int] -> [Int] -> [Piece]
+unidades semilla0 semilla1
+    | comparador semilla0 semilla1 == True = unidades (drop 4 semilla0) (drop 4 semilla1)
+    | otherwise          = (take 4 (soldados0 semilla0)) ++ (take 4 (soldados1 semilla1))
+
+{-
     playerPieces sirve para saber que piezas pertenecen al jugador.
     Recibe la lista de piezas y un Int correspondiente a la id del jugador.
     Retorna la lista de piezas del jugador.
@@ -53,7 +155,6 @@ moveName (xi,yi) (xf,yf) = let
 
 playerPieces :: [Piece] -> Int -> [Piece]
 playerPieces pcs n = [pc | pc@(p,x,y,k) <- pcs, p==n]
-
 
 {-
     drawBoard sirve para generar el String que se visualizará por pantalla.
@@ -69,4 +170,3 @@ drawBoard (sizeX,sizeY) drawf = let
     -- Dibujar el resto del tablero
     drawLine y    = show y ++ "\t" ++ concat [drawf (x,y) | x<-[0..sizeX-1]] ++ "\n"
     in concatMap drawLine [-1..sizeY-1]
-
